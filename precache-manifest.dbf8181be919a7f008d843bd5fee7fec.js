7246aecafbc4f95bef35c3828beba6729ba4736f
Mythireyan <mythireyanp@gmail.com>
2020-09-29 13:14:11 +0530

    Updates

---
+++ precache-manifest.dbf8181be919a7f008d843bd5fee7fec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e5caa5d3e59ca91ff21aa2cab362ef",
+    "url": "/Facebook-Messenger-clone/index.html"
+  },
+  {
+    "revision": "ceff9bcb6dba8da76ff5",
+    "url": "/Facebook-Messenger-clone/static/css/main.8e80bdfb.chunk.css"
+  },
+  {
+    "revision": "9cf7e8e3dc947014b2c7",
+    "url": "/Facebook-Messenger-clone/static/js/2.0308297f.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Facebook-Messenger-clone/static/js/2.0308297f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceff9bcb6dba8da76ff5",
+    "url": "/Facebook-Messenger-clone/static/js/main.320ea3f7.chunk.js"
+  },
+  {
+    "revision": "b6b1fa425177c87ef489",
+    "url": "/Facebook-Messenger-clone/static/js/runtime-main.9308bf74.js"
+  }
+]);
